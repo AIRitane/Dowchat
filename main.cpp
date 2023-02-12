444ce5d98b34dac291a651428af9cdc9fa6fb373
AIRitane <78357554+AIRitane@users.noreply.github.com>
2023-02-12 19:19:58 +0800

    实现微信多开功能

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 #include <TlHelp32.h>
 #include <string.h>
 #include <iostream>
+#include "main.h"
+#include <Shlwapi.h>
+#pragma comment(lib,"shlwapi.lib")
+
+//#define DEBUG
+
+/*DEBUG打印宏定义*/
+#ifdef DEBUG
+#define DeBugInfo(...) printf(__VA_ARGS__)
+#else
+#define DeBugInfo(...) ;
+#endif // DEBUG
 
 /**
  * @brief 进程提权
@@ -88,9 +100,9 @@ int GetProcIds(LPWSTR Name, DWORD* Pids)
 		if (Process32First(hSnap, &pe32))
 		{
 			do {
-				if (!_wcsicmp((const wchar_t*)Name, (const wchar_t *)pe32.szExeFile))
+				if (!_wcsicmp((const wchar_t*)Name, (const wchar_t*)pe32.szExeFile))
 				{
-					printf("%ls\n", pe32.szExeFile);
+					DeBugInfo("%ls\n", pe32.szExeFile);
 					if (Pids)
 					{
 						Pids[num++] = pe32.th32ProcessID;
@@ -104,26 +116,294 @@ int GetProcIds(LPWSTR Name, DWORD* Pids)
 	return num;
 }
 
-void test()
+/**
+ * @brief 得到微信PID
+ * @param Pids 微信pid缓存BUF
+ * @return 进程数量
+*/
+DWORD GetWeChatPid(DWORD* Pids)
 {
-	DWORD Pids[100] = { 0 };
-
 	DWORD Num = GetProcIds(ConvertLPSTRoLPWSTR("WeChat.exe"), Pids);
 	for (size_t i = 0; i < Num; i++)
 	{
-		printf("%d : %d\n",i,Pids[i]);
+		DeBugInfo("%d : %d\n", i, Pids[i]);
+	}
+
+	return Num;
+}
+
+/**
+ * @brief 复制句柄
+ * @param pid
+ * @param h
+ * @param flags
+ * @return
+*/
+HANDLE DuplicateHandleEx(DWORD pid, HANDLE h, DWORD flags)
+{
+	HANDLE hHandle = NULL;
+
+	HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+	if (hProc)
+	{
+		if (!DuplicateHandle(hProc,
+			(HANDLE)h, GetCurrentProcess(),
+			&hHandle, 0, FALSE, /*DUPLICATE_SAME_ACCESS*/flags))
+		{
+			hHandle = NULL;
+		}
+		CloseHandle(hProc);
+	}
+
+	return hHandle;
+}
+
+/**
+ * @brief 判断num个pid是否相等
+ * @param Pid
+ * @param Pids
+ * @param num
+ * @return
+*/
+BOOL IsTargetPid(DWORD Pid, DWORD* Pids, int num)
+{
+	for (int i = 0; i < num; i++)
+	{
+		if (Pid == Pids[i])
+		{
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+/**
+ * @brief 获取系统句柄信息
+ * @param pbuffer
+ * @return
+*/
+BOOL GetSystemHandleInfo(PVOID* pbuffer)
+{
+	NTSTATUS Status;
+	DWORD dwSize = 0;
+
+	//获取系统句柄信息
+	*pbuffer = VirtualAlloc(NULL, 0x1000, MEM_COMMIT, PAGE_READWRITE);;
+	if (!*pbuffer)
+	{
+		return FALSE;
+	}
+	Status = ZwQuerySystemInformation(SystemHandleInformation, *pbuffer, 0x1000, &dwSize);
+	DeBugInfo("dwSize = %d\n", dwSize);
+
+	if (!NT_SUCCESS(Status))
+	{
+		if (STATUS_INFO_LENGTH_MISMATCH != Status)
+		{
+			return FALSE;
+		}
+		else
+		{
+			// 这里大家可以保证程序的正确性使用循环分配稍好
+			if (NULL != *pbuffer)
+			{
+				VirtualFree(*pbuffer, 0, MEM_RELEASE);
+			}
+
+			if (dwSize * 2 > 0x4000000)  // MAXSIZE
+			{
+				return FALSE;
+			}
+
+			*pbuffer = VirtualAlloc(NULL, dwSize * 2, MEM_COMMIT, PAGE_READWRITE);
+
+			if (!*pbuffer)
+			{
+				return FALSE;
+			}
+
+			Status = ZwQuerySystemInformation(SystemHandleInformation, *pbuffer, dwSize * 2, NULL);
+
+			if (!NT_SUCCESS(Status))
+			{
+				return FALSE;
+			}
+		}
+	}
+
+	return TRUE;
+}
+
+/**
+ * @brief 删除微信单实例标志
+ * @param pHandleInfo
+ * @param Pids
+ * @param Num
+ * @return
+*/
+BOOL DeleteSingleWeChat(PSYSTEM_HANDLE_INFORMATION1* pHandleInfo, DWORD* Pids, int Num)
+{
+	NTSTATUS Status;
+	uint32_t nIndex = 0;
+	char szType[128] = { 0 };
+	char szName[512] = { 0 };
+	DWORD dwFlags = 0;
+	POBJECT_NAME_INFORMATION pNameInfo;
+	POBJECT_NAME_INFORMATION pNameType;
+
+	for (nIndex = 0; nIndex < (*pHandleInfo)->NumberOfHandles; nIndex++)
+	{
+		if (IsTargetPid((*pHandleInfo)->Handles[nIndex].UniqueProcessId, Pids, Num))
+		{
+			HANDLE hHandle = DuplicateHandleEx((*pHandleInfo)->Handles[nIndex].UniqueProcessId,
+				(HANDLE)(*pHandleInfo)->Handles[nIndex].HandleValue,
+				DUPLICATE_SAME_ACCESS
+			);
+			if (hHandle == NULL) continue;
+
+			Status = NtQueryObject(hHandle, ObjectNameInformation, szName, 512, &dwFlags);
+
+			if (!NT_SUCCESS(Status))
+			{
+				CloseHandle(hHandle);
+				continue;
+			}
+
+			Status = NtQueryObject(hHandle, ObjectTypeInformation, szType, 128, &dwFlags);
+
+			if (!NT_SUCCESS(Status))
+			{
+				CloseHandle(hHandle);
+				continue;
+			}
+
+			pNameInfo = (POBJECT_NAME_INFORMATION)szName;
+			pNameType = (POBJECT_NAME_INFORMATION)szType;
+
+			WCHAR TypName[1024] = { 0 };
+			WCHAR Name[1024] = { 0 };
+
+			wcsncpy_s(TypName, 1024, (WCHAR*)pNameType->Name.Buffer, pNameType->Name.Length / 2);
+			wcsncpy_s(Name, 1024, (WCHAR*)pNameInfo->Name.Buffer, pNameInfo->Name.Length / 2);
+
+			// 匹配是否为需要关闭的句柄名称
+			if (0 == wcscmp(TypName, L"Mutant"))
+			{
+				//WeChat_aj5r8jpxt_Instance_Identity_Mutex_Name
+				//if (wcsstr(Name, L"_WeChat_App_Instance_Identity_Mutex_Name"))
+				if (wcsstr(Name, L"_WeChat_") &&
+					wcsstr(Name, L"_Instance_Identity_Mutex_Name"))
+				{
+					CloseHandle(hHandle);
+
+					hHandle = DuplicateHandleEx((*pHandleInfo)->Handles[nIndex].UniqueProcessId,
+						(HANDLE)(*pHandleInfo)->Handles[nIndex].HandleValue,
+						DUPLICATE_CLOSE_SOURCE
+					);
+
+					if (hHandle)
+					{
+						printf("+ Patch wechat success!\n");
+						CloseHandle(hHandle);
+					}
+					else
+					{
+						printf("- Patch error: %d\n", GetLastError());
+					}
+
+					return FALSE;
+				}
+			}
+			else
+			{
+				//DeBugInfo("已删除微信单标志");
+			}
+
+			CloseHandle(hHandle);
+		}
+	}
+	return TRUE;
+}
+
+/**
+ * @brief 双开微信
+ * @return
+*/
+int PatchWeChat()
+{
+	DWORD Pids[100] = { 0 };
+	DWORD Num = 0;
+	PVOID pbuffer = NULL;
+
+	ElevatePrivileges();
+	Num = GetWeChatPid(Pids);
+	PSYSTEM_HANDLE_INFORMATION1 pHandleInfo;
+
+	if (Num == 0)
+	{
+		return 0;
+	}
+	if (!ZwQuerySystemInformation)
+	{
+		goto Exit0;
+	}
+
+	if (!GetSystemHandleInfo(&pbuffer))
+	{
+		DeBugInfo("找不到系统句柄信息\n");
+		goto Exit0;
+	}
+
+	pHandleInfo = (PSYSTEM_HANDLE_INFORMATION1)pbuffer;
+	//删除单实例标志
+	if (!DeleteSingleWeChat(&pHandleInfo, Pids, Num))
+	{
+		DeBugInfo("删除微信单标志失败\n");
+		goto Exit0;
+	}
+
+Exit0:
+	if (NULL != pbuffer)
+	{
+		VirtualFree(pbuffer, 0, MEM_RELEASE);
+	}
+
+	return 0;
+}
+
+/**
+ * @brief 打开微信app
+*/
+void OpenWeChat()
+{
+	//HKEY_CURRENT_USER\Software\Tencent\WeChat InstallPath = xx
+	HKEY hKey = NULL;
+	if (ERROR_SUCCESS != RegOpenKey(HKEY_CURRENT_USER, L"Software\\Tencent\\WeChat", &hKey))
+	{
+		return;
+	}
+
+	DWORD Type = REG_SZ;
+	WCHAR Path[MAX_PATH] = { 0 };
+	char EXEPath[MAX_PATH] = { 0 };
+	DWORD cbData = MAX_PATH * sizeof(WCHAR);
+	if (ERROR_SUCCESS != RegQueryValueEx(hKey, L"InstallPath", 0, &Type, (LPBYTE)Path, &cbData))
+	{
+		goto __exit;
+	}
+
+	DeBugInfo("%ls\n", Path);
+	sprintf_s(EXEPath, MAX_PATH-1,"%ls\\WeChat.exe", Path);
+
+	ShellExecute(NULL, L"open", ConvertLPSTRoLPWSTR(EXEPath), NULL, NULL, SW_SHOW);
+
+__exit:
+	if (hKey)
+	{
+		RegCloseKey(hKey);
 	}
 }
 
-int PatchWeChat()
-{
-
-	ElevatePrivileges();
-	test();
-
-	return 0;
-
-}
 int main(int argc, char* argv[])
 {
 	printf("------------------------------------------------------------\n");
@@ -133,4 +413,5 @@ int main(int argc, char* argv[])
 	printf("------------------------------------------------------------\n\n");
 
 	PatchWeChat();
+	OpenWeChat();
 }
---
+++ main.h
@@ -0,0 +1,120 @@
+#pragma once
+
+/*******************************************************************ZwQuerySystemInformation******************************************************/
+#define NT_SUCCESS(Status) ((NTSTATUS)(Status) >= 0)
+#define STATUS_INFO_LENGTH_MISMATCH ((NTSTATUS)0xC0000004L)
+
+typedef enum _SYSTEM_INFORMATION_CLASS {
+	SystemBasicInformation,              // 0        Y        N
+	SystemProcessorInformation,          // 1        Y        N
+	SystemPerformanceInformation,        // 2        Y        N
+	SystemTimeOfDayInformation,          // 3        Y        N
+	SystemNotImplemented1,               // 4        Y        N
+	SystemProcessesAndThreadsInformation, // 5       Y        N
+	SystemCallCounts,                    // 6        Y        N
+	SystemConfigurationInformation,      // 7        Y        N
+	SystemProcessorTimes,                // 8        Y        N
+	SystemGlobalFlag,                    // 9        Y        Y
+	SystemNotImplemented2,               // 10       Y        N
+	SystemModuleInformation,             // 11       Y        N
+	SystemLockInformation,               // 12       Y        N
+	SystemNotImplemented3,               // 13       Y        N
+	SystemNotImplemented4,               // 14       Y        N
+	SystemNotImplemented5,               // 15       Y        N
+	SystemHandleInformation,             // 16       Y        N
+	SystemObjectInformation,             // 17       Y        N
+	SystemPagefileInformation,           // 18       Y        N
+	SystemInstructionEmulationCounts,    // 19       Y        N
+	SystemInvalidInfoClass1,             // 20
+	SystemCacheInformation,              // 21       Y        Y
+	SystemPoolTagInformation,            // 22       Y        N
+	SystemProcessorStatistics,           // 23       Y        N
+	SystemDpcInformation,                // 24       Y        Y
+	SystemNotImplemented6,               // 25       Y        N
+	SystemLoadImage,                     // 26       N        Y
+	SystemUnloadImage,                   // 27       N        Y
+	SystemTimeAdjustment,                // 28       Y        Y
+	SystemNotImplemented7,               // 29       Y        N
+	SystemNotImplemented8,               // 30       Y        N
+	SystemNotImplemented9,               // 31       Y        N
+	SystemCrashDumpInformation,          // 32       Y        N
+	SystemExceptionInformation,          // 33       Y        N
+	SystemCrashDumpStateInformation,     // 34       Y        Y/N
+	SystemKernelDebuggerInformation,     // 35       Y        N
+	SystemContextSwitchInformation,      // 36       Y        N
+	SystemRegistryQuotaInformation,      // 37       Y        Y
+	SystemLoadAndCallImage,              // 38       N        Y
+	SystemPrioritySeparation,            // 39       N        Y
+	SystemNotImplemented10,              // 40       Y        N
+	SystemNotImplemented11,              // 41       Y        N
+	SystemInvalidInfoClass2,             // 42
+	SystemInvalidInfoClass3,             // 43
+	SystemTimeZoneInformation,           // 44       Y        N
+	SystemLookasideInformation,          // 45       Y        N
+	SystemSetTimeSlipEvent,              // 46       N        Y
+	SystemCreateSession,                 // 47       N        Y
+	SystemDeleteSession,                 // 48       N        Y
+	SystemInvalidInfoClass4,             // 49
+	SystemRangeStartInformation,         // 50       Y        N
+	SystemVerifierInformation,           // 51       Y        Y
+	SystemAddVerifier,                   // 52       N        Y
+	SystemSessionProcessesInformation    // 53       Y        N
+} SYSTEM_INFORMATION_CLASS;
+
+typedef struct _SYSTEM_HANDLE_TABLE_ENTRY_INFO {
+	USHORT UniqueProcessId;
+	USHORT CreatorBackTraceIndex;
+	UCHAR ObjectTypeIndex;
+	UCHAR HandleAttributes;
+	USHORT HandleValue;
+	PVOID Object;
+	ULONG GrantedAccess;
+} SYSTEM_HANDLE_TABLE_ENTRY_INFO, * PSYSTEM_HANDLE_TABLE_ENTRY_INFO;
+
+typedef struct _SYSTEM_HANDLE_INFORMATION1 {
+	ULONG NumberOfHandles;
+	SYSTEM_HANDLE_TABLE_ENTRY_INFO Handles[1];
+} SYSTEM_HANDLE_INFORMATION1, * PSYSTEM_HANDLE_INFORMATION1;
+
+typedef enum _OBJECT_INFORMATION_CLASS {
+	ObjectBasicInformation,
+	ObjectNameInformation,
+	ObjectTypeInformation,
+	ObjectAllInformation,
+	ObjectDataInformation
+} OBJECT_INFORMATION_CLASS;
+
+typedef NTSTATUS(NTAPI* NTQUERYOBJECT)(
+	_In_opt_   HANDLE Handle,
+	_In_       OBJECT_INFORMATION_CLASS ObjectInformationClass,
+	_Out_opt_  PVOID ObjectInformation,
+	_In_       ULONG ObjectInformationLength,
+	_Out_opt_  PULONG ReturnLength
+
+	);
+typedef NTSTATUS
+(NTAPI* ZWQUERYSYSTEMINFORMATION)(
+	IN SYSTEM_INFORMATION_CLASS SystemInformationClass,
+	OUT PVOID SystemInformation,
+	IN ULONG SystemInformationLength,
+	OUT PULONG ReturnLength OPTIONAL
+	);
+
+typedef struct
+{
+	USHORT Length;
+	USHORT MaxLen;
+	USHORT* Buffer;
+}UNICODE_STRING, * PUNICODE_STRING;
+
+typedef struct _OBJECT_NAME_INFORMATION {
+	UNICODE_STRING Name;
+} OBJECT_NAME_INFORMATION, * POBJECT_NAME_INFORMATION;
+
+ZWQUERYSYSTEMINFORMATION ZwQuerySystemInformation = (ZWQUERYSYSTEMINFORMATION)GetProcAddress(GetModuleHandleA("ntdll.dll"), "ZwQuerySystemInformation");
+
+
+
+
+/*******************************************************************NtQueryObject******************************************************/
+NTQUERYOBJECT    NtQueryObject = (NTQUERYOBJECT)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtQueryObject");
